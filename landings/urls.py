26a1367d7933cd3fd763e9b4a2c85a6a1fc40e9c
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2021-01-28 08:59:52 -0500

    Basic caesar cipher implemented

--- landings/urls.py
+++ landings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # , include
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.caesar, name='caesar'),
 ]
--- landings/views.py
+++ landings/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+import string
 
+def caesar(request):
+    # Courtesy of Real Python: https://realpython.com/python-practice-problems/
+    plain_text = request.GET['original_entry']
+    shift_num = request.GET['shift_num']
+    shift_num = int(shift_num)
+    letters = string.ascii_lowercase
+    mask = letters[shift_num:] + letters[:shift_num]
+    trantab = str.maketrans(letters, mask)
+    output_text = plain_text.translate(trantab)
+    context = {'output_text': output_text, 'original_entry': plain_text,}
+    return render(request, 'landings/home.html', context)
+
+
+# print(caesar("abcdefghijklmnopqrstuvwxyz", 3))
 # Create your views here.
 
-def home(request):
-    return render(request, 'landings/home.html')
+# def home(request):    
+# return render(request, 'landings/home.html')
